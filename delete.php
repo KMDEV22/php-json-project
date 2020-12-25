111ecb475b4367e12614923aa2dd8704978f8f83
KMDEV22 <70811830+KMDEV22@users.noreply.github.com>
2020-12-25 21:51:34 +0530

    Adding Delete functionality!

--- delete.php
+++ delete.php
@@ -2,16 +2,14 @@
     $myvar = 'states.json';
     $filecontent = json_decode(file_get_contents($myvar));
     $states = $filecontent->maharastra;
-    $gotvars = $_POST["states"];
+    $gotvars = $_POST[ array_keys($_POST)[0]];
     echo $gotvars;
     if(file_exists($myvar)){
         $file = fopen($myvar,'w');
-        if(!in_array($gotvars,$states)){
-            $can_push = true;
-            foreach($states as $st){
-                if(in_array($st, ));
-            }
-            array_push($states,$gotvars);
+        if(in_array($gotvars,$states)){
+            echo 'came inside if';
+            $index=array_search($gotvars,$states);
+            array_splice($states,$index,1);
         }
         
         $filecontent->maharastra = $states;
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-/<?php
+<?php
     $datafile = "states.json";
     $file=fopen($datafile, 'r');
     if($file != NULL || $file != false){
@@ -8,9 +8,11 @@
         echo $filesize;
         $filecontents = fread($file, $filesize);
         fclose($file);
+        $ctr=1;
+        echo '<link rel ="stylesheet" href="./style.css">';
         echo '<ul>';
         foreach(json_decode($filecontents)->maharastra as $st){
-            echo "<li>$st</li>";
+            echo "<li><form action='delete.php' method='POST'><span>$st</span><input name=\"state".$ctr++."\" value=\"$st\" type=\"hidden\"><button class='btn'><i  class='delete'></i></button></form></li>";
         }
         echo '</ul>';
         echo "$filecontents";
@@ -18,12 +20,12 @@
 
         echo '<h3>display an input box to add states</h3>';
         echo '<form action="save.php" method="POST"><input name="states"><br><button>Add</button></form> <br><br>';
-        print_r(array_values(json_decode($filecontents)->maharastra));
 
         
 
     }else{
         echo 'file not found <br>';
     }
-    
+
+    echo "<script>function show(item){  alert(item)     }</script>"
 ?>
